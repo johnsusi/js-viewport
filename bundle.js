d31d4aca85d6533e725d2f48175156235afd9c4d
John Susi <john@susi.se>
2015-06-13 17:17:31 +0200

    Improved support for fit in view/rect

--- bundle.js
+++ bundle.js
@@ -14348,10 +14348,11 @@ Math.sign = Math.sign || function(x) {
   }
   return x > 0 ? 1 : -1;
 };
-function zoom_to_point(transform, x, y, factor) {}
 $(function() {
   var image = undefined;
+  var map = $('#map')[0];
   var transform = mat2d.create();
+  var pin_fit_in_view = true;
   mat2d.translate(transform, transform, [320, 0]);
   var last_frame = {
     timestamp: undefined,
@@ -14369,7 +14370,6 @@ $(function() {
     var fps = 0.7 * 1000 / timestamp_delta + 0.3 * last_frame.fps;
     last_frame.fps = 1000 / timestamp_delta;
     info('fps', fps.toFixed(0));
-    var map = $('#map')[0];
     var overlay = $('#overlay')[0];
     var perf = performance.now();
     var g = map.getContext('2d');
@@ -14394,13 +14394,11 @@ $(function() {
     perf = performance.now() - perf;
     info('redraw', perf.toFixed(2) + 'ms');
     info('transform', '<br>' + '    ' + transform[0].toFixed(2) + ' ' + transform[1].toFixed(2) + '<br>' + '    ' + transform[2].toFixed(2) + ' ' + transform[3].toFixed(2) + '<br>' + '    ' + transform[4].toFixed(2) + ' ' + transform[5].toFixed(2));
-    invalidate();
   }
   function invalidate() {
     requestAnimationFrame(redraw);
   }
   $(window).on('resize', (function(event) {
-    console.log('resize');
     $('canvas').each(function() {
       this.width = $(this).width();
       this.height = $(this).height();
@@ -14415,6 +14413,10 @@ $(function() {
       g.textAlign = 'center';
       g.fillStyle = "rgb(255,255,255)";
       g.fillRect(0, 0, canvas.width, canvas.height);
+      g.lineWidth = 2.0;
+      g.strokeStyle = "rgb(127,0,0)";
+      g.strokeRect(150, 150, 300, 300);
+      g.strokeStyle = "rgb(0,0,0)";
       g.fillStyle = "rgb(0,0,0)";
       for (var y = 0; y < 1024; y += 256) {
         for (var x = 0; x < 1024; x += 256) {
@@ -14426,25 +14428,20 @@ $(function() {
       }
       return canvas;
     }();
+    if (pin_fit_in_view)
+      fit_in_view();
     invalidate();
   })).resize();
   var drag = {
     active: false,
     x: undefined,
-    y: undefined,
-    t: mat2d.create()
-  };
-  var zoom = {
-    timestamp: undefined,
-    factor: undefined,
-    t: mat2d.create()
+    y: undefined
   };
   $('#map').on('mousedown', (function(event) {
     event.preventDefault();
     drag.active = true;
     drag.x = event.pageX;
     drag.y = event.pageY;
-    mat2d.copy(drag.t, transform);
   })).on('mouseup', (function(event) {
     event.preventDefault();
     drag.active = false;
@@ -14459,11 +14456,13 @@ $(function() {
     mat2d.multiply(transform, t, transform);
     drag.x += dx;
     drag.y += dy;
+    pin_fit_in_view = false;
     invalidate();
   })).on('mousewheel', (function(event) {
     event.preventDefault();
     if (drag.active)
       return;
+    pin_fit_in_view = false;
     var dx = (event.pageX);
     var dy = (event.pageY);
     var k = (event.deltaY * event.deltaFactor) / 120;
@@ -14474,6 +14473,30 @@ $(function() {
     mat2d.translate(t, t, [-dx, -dy]);
     mat2d.multiply(transform, t, transform);
     invalidate();
+  }));
+  function fit_in_view() {
+    zoom_to_rect(0, 0, image.width, image.height);
+    pin_fit_in_view = true;
+  }
+  function zoom_to_rect(x, y, width, height) {
+    var iw = width;
+    var ih = height;
+    var vw = map.width;
+    var vh = map.height;
+    var scalex = vw / iw;
+    var scaley = vh / ih;
+    var scale = Math.min(scalex, scaley);
+    var dx = Math.abs(vw - iw * scale);
+    var dy = Math.abs(vh - ih * scale);
+    transform = mat2d.create();
+    var tx = scalex > scaley ? (dx - 2 * x * scale) / (2) : -x * scale;
+    var ty = scaley > scalex ? (dy - 2 * y * scale) / (2) : -y * scale;
+    mat2d.mul(transform, transform, [scale, 0, 0, scale, tx, ty]);
+    invalidate();
+  }
+  $('#fit_in_view').click(fit_in_view);
+  $('#fit_in_rect').click((function(event) {
+    zoom_to_rect(150, 150, 300, 300);
   }));
 });
 
--- src/index.js
+++ src/index.js
@@ -12,17 +12,12 @@ Math.sign = Math.sign || function(x) {
   return x > 0 ? 1 : -1;
 }
 
-
-function zoom_to_point(transform, x, y, factor) {
-
-
-
-}
-
 $( function() {
 
   var image = undefined;
+  var map = $('#map')[0];
   var transform = mat2d.create();
+  var pin_fit_in_view = true;
 
   mat2d.translate(transform, transform, [320, 0]);
 
@@ -51,7 +46,6 @@ $( function() {
 
     info('fps', fps.toFixed(0));
 
-    var map = $('#map')[0];
     var overlay = $('#overlay')[0];
     var perf = performance.now();
 
@@ -69,8 +63,6 @@ $( function() {
       var g = overlay.getContext('2d');
       g.setTransform(1, 0, 0, 1, 0, 0);
       g.clearRect(0, 0, overlay.width, overlay.height);
-//      g.fillStyle = "rgba(255, 127, 0, 0.5)";
-//      g.fillRect(0, 0, overlay.width, overlay.height);
 
       var p0 = vec2.fromValues(0, 0);
       var p1 = vec2.fromValues(image.width, image.height);
@@ -78,7 +70,7 @@ $( function() {
       var scale = overlay.width / map.width;
       mat2d.scale(t2, t2, [scale, scale]);
       mat2d.multiply(t2, t2, transform);
-      
+
       g.setTransform(
         t2[0], t2[1],
         t2[2], t2[3],
@@ -99,14 +91,13 @@ $( function() {
       '    ' + transform[4].toFixed(2) + ' ' + transform[5].toFixed(2));
 
 
-    invalidate();
+//    invalidate();
   }
 
   function invalidate() { requestAnimationFrame(redraw); }
 
   $(window).on('resize', event => {
 
-    console.log('resize');
     $('canvas').each(function () {
       this.width = $(this).width();
       this.height = $(this).height();
@@ -122,7 +113,13 @@ $( function() {
       g.textAlign = 'center';
       g.fillStyle="rgb(255,255,255)";
       g.fillRect(0, 0, canvas.width, canvas.height);
+
+      g.lineWidth = 2.0;
+      g.strokeStyle ="rgb(127,0,0)";
+      g.strokeRect(150, 150, 300, 300);
+      g.strokeStyle="rgb(0,0,0)";
       g.fillStyle="rgb(0,0,0)";
+
       for (var y = 0;y < 1024;y += 256) {
         for (var x = 0;x < 1024; x += 256) {
           g.strokeRect(x, y, x + 255, y + 255);
@@ -134,6 +131,7 @@ $( function() {
     }();
 
 
+    if (pin_fit_in_view) fit_in_view();
     invalidate();
 
   }).resize();
@@ -142,21 +140,13 @@ $( function() {
     active: false,
     x: undefined,
     y: undefined,
-    t: mat2d.create(),
   };
-
-  var zoom = {
-    timestamp: undefined,
-    factor: undefined,
-    t: mat2d.create(),
-  }
 
   $('#map').on('mousedown', event => {
     event.preventDefault();
     drag.active = true;
     drag.x = event.pageX;
     drag.y = event.pageY;
-    mat2d.copy(drag.t, transform);
   }).on('mouseup', event => {
     event.preventDefault();
     drag.active = false;
@@ -170,10 +160,12 @@ $( function() {
     mat2d.multiply(transform, t, transform);
     drag.x += dx;
     drag.y += dy;
+    pin_fit_in_view = false;
     invalidate();
   }).on('mousewheel', event => {
     event.preventDefault();
     if (drag.active) return;
+    pin_fit_in_view = false;
 
     var dx = (event.pageX);
     var dy = (event.pageY);
@@ -189,6 +181,43 @@ $( function() {
 
     invalidate();
 
+  });
+
+  function fit_in_view() {
+    zoom_to_rect(0, 0, image.width, image.height);
+    pin_fit_in_view = true;
+  }
+
+
+  function zoom_to_rect(x, y, width, height) {
+    var iw = width;
+    var ih = height;
+    var vw = map.width;
+    var vh = map.height;
+
+    var scalex = vw / iw;
+    var scaley = vh / ih;
+    var scale = Math.min(scalex, scaley);
+
+    var dx = Math.abs(vw - iw*scale);
+    var dy = Math.abs(vh - ih*scale);
+
+    transform = mat2d.create();
+
+    var tx = scalex > scaley ? (dx - 2*x*scale) / (2) : -x * scale;
+    var ty = scaley > scalex ? (dy - 2*y*scale) / (2) : -y * scale;
+
+    mat2d.mul(transform, transform, [
+      scale, 0,
+      0, scale,
+      tx, ty
+    ]);
+    invalidate();
+  }
+
+  $('#fit_in_view').click(fit_in_view);
+  $('#fit_in_rect').click(event => {
+    zoom_to_rect(150, 150, 300, 300);
   });
 
 });
